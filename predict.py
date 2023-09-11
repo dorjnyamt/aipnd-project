1e7e5578fc99ac460c322ee588b60e316a581d7c
Dorjnyam Tumur-Ochir <dorjnyam.to@gmail.com>
2023-09-11 19:06:34 +0800

    DONE

---
+++ predict.py
@@ -0,0 +1,121 @@
+# Imports here
+import os
+import argparse
+import json
+
+import torch
+import torch.nn as nn
+from torchvision import models
+import numpy as np
+from PIL import Image
+
+
+def process_image(image_path):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    with Image.open(image_path) as image:
+        image.thumbnail((256, 256))
+        width, height = image.size
+        left = (width - 224)/2
+        top = (height - 224)/2
+        right = (width + 224)/2
+        bottom = (height + 224)/2
+        image = image.crop((left, top, right, bottom))
+        np_img = np.array(image) / 255
+        np_img = (np_img - np.array([0.485, 0.456, 0.406])
+                  ) / np.array([0.229, 0.224, 0.225])
+        np_img = np_img.transpose((2, 0, 1))
+        return np_img
+
+
+def predict(image_path, model, topk=5):
+    """Predict the class (or classes) of an image using a trained deep learning model."""
+    image = process_image(image_path)
+    image_tensor = torch.from_numpy(image).type(torch.Tensor)
+    image_tensor = image_tensor.to(device)
+    model_input = image_tensor.unsqueeze(0)
+    probs = torch.exp(model(model_input))
+    top_probs, top_labs = probs.topk(topk)
+    top_probs = top_probs.detach().cpu().numpy().tolist()[0]
+    top_labs = top_labs.detach().cpu().numpy().tolist()[0]
+
+    idx_to_class = {val: key for key, val in model.class_to_idx.items()}
+    classes = [idx_to_class[lab] for lab in top_labs]
+    return top_probs, classes
+
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    if checkpoint['arch'] == 'vgg16':
+        model = models.vgg16()
+        model.classifier = nn.Sequential(
+            nn.Linear(
+                in_features=model.classifier[0].in_features,
+                out_features=checkpoint['hidden_units'],
+            ),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(in_features=checkpoint['hidden_units'],
+                      out_features=checkpoint['hidden_units']),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(
+                in_features=checkpoint['hidden_units'],
+                out_features=checkpoint['n_features'],
+            ),
+            nn.LogSoftmax(dim=1),
+        )
+    elif checkpoint['arch'] == 'resnet18':
+        model = models.resnet18()
+        model.fc = nn.Sequential(
+            nn.Linear(
+                in_features=model.fc.in_features,
+                out_features=checkpoint['hidden_units'],
+            ),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(in_features=checkpoint['hidden_units'],
+                      out_features=checkpoint['hidden_units']),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(
+                in_features=checkpoint['hidden_units'],
+                out_features=checkpoint['n_features'],
+            ),
+            nn.LogSoftmax(dim=1),
+        )
+    else:
+        raise ValueError(f"Model architecture is not supported")
+        
+    model.class_to_idx = checkpoint["class_to_idx"]
+    model.load_state_dict(checkpoint["model_state_dict"])
+    return model
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Predict flower name from an image')
+    parser.add_argument('image_path')
+    parser.add_argument('chkpt_path')
+    parser.add_argument('--top_k', type=int, default=1,
+                        help='Number of top predicted classes')
+    parser.add_argument('--category_names', type=str)
+    parser.add_argument('--gpu', action='store_true')
+    args = parser.parse_args()
+    print(vars(args))
+    device = torch.device(
+        "cuda:0" if torch.cuda.is_available() and args.gpu else "cpu")
+    loaded_model = load_checkpoint(args.chkpt_path)
+    loaded_model.to(device)
+    probs, classes = predict(args.image_path, loaded_model, args.top_k)
+    if args.category_names:
+        with open(args.category_names, 'r') as f:
+            cat_to_name = json.load(f)
+            names = [cat_to_name[cat] for cat in classes]
+            print(names)
+            print(probs)
+            
+    else:
+        print(classes)
+        print(probs)
+        
--- train.py
+++ train.py
@@ -10,91 +10,15 @@ from torchvision import models
 from datasets import image_datasets, image_loaders
 import utils
 
-
-def train_model(
-    savedir,
-    model,
-    criterion,
-    optimizer,
-    scheduler,
-    epochs,
-    dataloaders,
-    dataset_sizes,
-    device,
-):
-    # Create a temporary directory to save training checkpoints
-    best_model_params_path = os.path.join(savedir, "model.pth")
-
-    # torch.save(model.state_dict(), best_model_params_path)
-    utils.save_model(epochs, model, optimizer, criterion)
-    best_acc = 0.0
-
-    for epoch in range(epochs):
-        print(f"Epoch {epoch + 1}/{epochs}")
-        print("-" * 10)
-
-        # Each epoch has a training and validation phase
-        for phase in ["train", "valid"]:
-            if phase == "train":
-                model.train()  # Set model to training mode
-            else:
-                model.eval()  # Set model to evaluate mode
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == "train"):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-
-                    # backward + optimize only if in training phase
-                    if phase == "train":
-                        loss.backward()
-                        optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-            if phase == "train":
-                scheduler.step()
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
-
-            print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
-
-            # deep copy the model
-            if phase == "valid" and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                torch.save(model.state_dict(), best_model_params_path)
-
-        print()
-
-    print(f"Best val Acc: {best_acc:4f}")
-
-    # load best model weights
-    model.load_state_dict(torch.load(best_model_params_path))
-    return model
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train a network")
+    parser = argparse.ArgumentParser(
+        description="Finetune a VGG16 or ResNet18 on your own data")
     parser.add_argument("data_directory")
     parser.add_argument(
-        "--save_dir", type=str, default="out", help="Set directory to save checkpoints"
+        "--save_dir", type=str, default="outputs", help="Set directory to save checkpoints"
     )
-    parser.add_argument("--arch", type=str, default="vgg16", help="Choose architecture")
+    parser.add_argument("--arch", type=str, default="vgg16",
+                        help="Choose architecture")
     parser.add_argument("--learning_rate", type=float, default=0.01)
     parser.add_argument("--hidden_units", type=int, default=512)
     parser.add_argument("--epochs", type=int, default=10)
@@ -105,42 +29,107 @@ if __name__ == "__main__":
 
     os.makedirs(args.save_dir, exist_ok=True)
     root_dir = args.data_directory
-    image_datasets = image_datasets(root_dir)
-    dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "valid", "test"]}
-    dataloaders = image_loaders(image_datasets)
-    print(image_datasets)
-    n_features = len(image_datasets["train"].classes)
+    datasets = image_datasets(root_dir)
+    dataset_sizes = {x: len(datasets[x])
+                     for x in ["train", "valid", "test"]}
+    dataloaders = image_loaders(datasets)
+    n_features = len(datasets["train"].classes)
 
-    config = ""
+    # PREP MODEL
     if args.arch == "vgg16":
         weights = models.VGG16_Weights.DEFAULT
         model = models.vgg16(weights=weights)
-        model.classifier[6] = nn.Linear(
-            in_features=model.classifier[6].in_features, out_features=n_features
+        for param in model.parameters():
+            param.requires_grad = False
+        model.classifier = nn.Sequential(
+            nn.Linear(
+                in_features=model.classifier[0].in_features, out_features=args.hidden_units),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(in_features=args.hidden_units,
+                      out_features=args.hidden_units),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(in_features=args.hidden_units, out_features=n_features),
+            nn.LogSoftmax(dim=1)
         )
+        optimizer = optim.SGD(params=model.classifier.parameters(),
+                          lr=args.learning_rate, momentum=0.9)
     elif args.arch == "resnet18":
         weights = models.ResNet18_Weights.DEFAULT
         model = models.resnet18(weights=weights)
-        model.fc = nn.Linear(in_features=model.fc_in_features, out_features=n_features)
+        for param in model.parameters():
+            param.requires_grad = False
+        model.fc = nn.Sequential(
+            nn.Linear(
+                in_features=model.fc.in_features, out_features=args.hidden_units),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(in_features=args.hidden_units,
+                      out_features=args.hidden_units),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(in_features=args.hidden_units, out_features=n_features),
+            nn.LogSoftmax(dim=1)
+        )
+        optimizer = optim.SGD(params=model.fc.parameters(),
+                          lr=args.learning_rate, momentum=0.9)
     else:
         raise ValueError(f"Model architecture {args.arch} is not supported")
-    device = torch.device("cuda:0" if torch.cuda.is_available() and args.gpu else "cpu")
-    for param in model.parameters():
-        param.requires_grad = False
+    print(model)
+    device = torch.device(
+        "cuda:0" if torch.cuda.is_available() and args.gpu else "cpu")
 
     model = model.to(device)
-    optimizer = optim.Adam(model.classifier.parameters(), lr=args.learning_rate)
-    criterion = nn.CrossEntropyLoss()
-    exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+    criterion = nn.NLLLoss()
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-    model = train_model(
-        args.save_dir,
-        model,
-        criterion,
-        optimizer,
-        exp_lr_scheduler,
-        args.epochs,
-        dataloaders,
-        dataset_sizes,
-        device,
-    )
+    # TRAIN MODEL
+    print(f'TRAINING FOR {args.epochs} EPOCHS.')
+    for epoch in range(args.epochs):
+        for phase in ["train", "valid"]:
+            if phase == "train":
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+                with torch.set_grad_enabled(phase == "train"):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == "train":
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print(
+                f"Epoch {epoch + 1}/{args.epochs} {phase} loss: {epoch_loss:.4f} acc: {epoch_acc:.4f}")
+
+    # SAVE MODEL
+    model.class_to_idx = datasets['train'].class_to_idx
+    savepath = os.path.join(args.save_dir, f'{args.arch}-{args.data_directory}model.pt')
+    torch.save({
+        'arch': args.arch,
+        'model_state_dict': model.state_dict(),
+        'class_to_idx': model.class_to_idx,
+        'hidden_units': args.hidden_units,
+        'n_features': n_features
+    }, savepath)
+
+    print(f'TRAINING COMPLETE. MODEL SAVED AT {savepath}.')
